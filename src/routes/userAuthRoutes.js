db3523fe48d4122b5c3351f22543bc21ea7c5232
Kishan Jaiswal <jaiswalkishan09@gmail.com>
2022-10-09 10:35:08 +0545

    backend completed

--- src/routes/userAuthRoutes.js
+++ src/routes/userAuthRoutes.js
@@ -4,6 +4,7 @@ const userAuthRouter= express.Router();
 const { getUserDetails, updateUserDetails,deleteUser,verifyMessage } = require("../controllers/userAuthControllers");
 const auth=require("../middlewares/auth")
 
+
 userAuthRouter.get("/getuserDetails",auth,getUserDetails);
 
 userAuthRouter.put("/updateUserDetails",auth,updateUserDetails);
